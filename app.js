02ad1e5e1c886caa8bffe5edee2eb401541f016d
kimsejune <tpwns1088@gmail.com>
2021-12-26 11:50:36 +0900

    tsconfig lib 설정법

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
-var watch = "watch me";
-// tsc app.ts --watch
-console.log(watch);
+const button = document.querySelector("button"); // DOM 값을 찾아낸다.
+button.addEventListener("click", () => {
+    console.log("Clicked!");
+}); // TypeScript가 버튼을 감지해 낼 수 있을지 확신을 못하여 에러를 표시한다.
