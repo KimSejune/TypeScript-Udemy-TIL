5e4290735e37716d39af7ca86116ac27b3c91afc
kimsejune <tpwns1088@gmail.com>
2022-02-20 14:55:44 +0900

    Decorator
    Class Decorator
    Decorator Factory
    Decorator 여러개 적용
    Property Decorator

--- dist/app.js
+++ dist/app.js
@@ -1,54 +1,73 @@
 "use strict";
-function merge(objA, objB) {
-    return Object.assign(objA, objB);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(logString) {
+    console.log("LOGGER FACTORY");
+    return function (constructor) {
+        console.log("Rendering logger");
+        console.log(logString);
+        console.log(constructor);
+    };
 }
-const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
-console.log(mergedObj);
-function countAndDescribe(element) {
-    let descriptionText = "Got no value.";
-    if (element.length === 1) {
-        descriptionText = "Got 1 element.";
-    }
-    else if (element.length > 1) {
-        descriptionText = `Got ${element.length} elements.`;
-    }
-    return [element, descriptionText];
-}
-console.log(countAndDescribe("Hi there!"));
-console.log(countAndDescribe(["Sports", "Cooking"]));
-function extractAndConvert(obj, key) {
-    return obj[key];
-}
-console.log(extractAndConvert({ name: "Sejune" }, "name"));
-class DataStorage {
-    constructor() {
-        this.data = [];
-    }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
+function WithTemplate(template, hookId) {
+    console.log("TEMPLATE FACTORY");
+    return function (constructor) {
+        console.log("Rendering template");
+        const p = new constructor();
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").textContent = p.name;
         }
-        this.data.splice(this.data.indexOf(item), 1);
+    };
+}
+let Person = class Person {
+    constructor() {
+        this.name = "Max";
+        console.log("Creating person object...");
     }
-    getItems() {
-        return [...this.data];
+};
+Person = __decorate([
+    Logger("LOGGING"),
+    WithTemplate("<h1>My Person Object</h1>", "app")
+], Person);
+const person = new Person();
+console.log(person);
+function Log(target, propertyName) {
+    console.log("Property decorator!");
+    console.log(target, propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("Invalid Prive - should be positive!");
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
     }
 }
-const textStorage = new DataStorage();
-textStorage.addItem("Max");
-textStorage.addItem("Menu");
-textStorage.removeItem("max");
-console.log(textStorage.getItems());
-const numberStorage = new DataStorage();
-function createCourseGoal(title, description, date) {
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal;
-}
-const names = ["Max", "Sports"];
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log
+], Product.prototype, "_price", void 0);
+__decorate([
+    Log
+], Product.prototype, "price", null);
+__decorate([
+    Log
+], Product.prototype, "getPriceWithTax", null);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,108 +1,63 @@
-// const names: Array<string> = []; // string[]
-// names[0].split(" ");
-
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("This is End!");
-//   }, 2000);
-// });
-
-// promise.then((data) => {
-//   data.split(" ");
-// });
-
-// 2개의 객체를 병합하고 새 객체를 반환하는 함수.
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
+  return function (constructor: Function) {
+    console.log("Rendering logger");
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
-console.log(mergedObj);
-
-interface Lengthy {
-  length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = "Got no value.";
-  if (element.length === 1) {
-    descriptionText = "Got 1 element.";
-  } else if (element.length > 1) {
-    descriptionText = `Got ${element.length} elements.`;
-  }
-
-  return [element, descriptionText];
-}
-
-console.log(countAndDescribe("Hi there!"));
-console.log(countAndDescribe(["Sports", "Cooking"]));
-// console.log(countAndDescribe(10); // number은 length property가 없어서 호출되지 않는다.
-
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return obj[key];
-}
-
-console.log(extractAndConvert({ name: "Sejune" }, "name"));
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    // T이 object, array이면 indexof가 동작하지 않는다.
-    // JS는 주소를 찾아보지만 찾을 수 없어서 (-1)이 리턴되어 마지막값을 지워버린다.
-    if (this.data.indexOf(item) === -1) {
-      return;
+function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
+  return function (constructor: any) {
+    console.log("Rendering template");
+    const p = new constructor();
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
-    this.data.splice(this.data.indexOf(item), 1);
-  }
+  };
+}
 
-  getItems() {
-    return [...this.data];
+@Logger("LOGGING")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "Max";
+  constructor() {
+    console.log("Creating person object...");
   }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem("Max");
-textStorage.addItem("Menu");
-textStorage.removeItem("max");
-console.log(textStorage.getItems());
+const person = new Person();
 
-const numberStorage = new DataStorage<number>();
+console.log(person);
 
-// const objStorage = new DataStorage<object>();
-// const maxObj = { name: "Max" };
-// objStorage.addItem(maxObj);
-// objStorage.addItem({ name: "Menu" });
-// objStorage.removeItem(maxObj);
-// console.log(objStorage.getItems());
-// {
-//   "name": "Max"
-// }
-
-interface CourseGoal {
+// target => Property Target을 나타내며 object 구조를 가진다
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+class Product {
+  @Log
   title: string;
-  description: string;
-  completeUntil: Date;
-}
+  @Log
+  private _price: number;
 
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  @Log
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid Prive - should be positive!");
+    }
+  }
+  @Log
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
 }
-
-const names: Readonly<string[]> = ["Max", "Sports"];
-// names.push("Menu");
