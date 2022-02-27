fed4dd0fa79f6860bd7a9896d37137ddb0549509
kimsejune <tpwns1088@gmail.com>
2022-02-27 17:25:28 +0900

    Autobind Decorator Validation Decorator

--- src/app.ts
+++ src/app.ts
@@ -110,3 +110,131 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function Autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  // 항상 this 키워드를 이 메서드가 아직 속해 있는 객체로 설정하려고한다.
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      // this는 getter Method를 대체한다.
+      // getter method를 트리거하는 것을 참조한다.
+      // 따라서 getter mathod 대신 this가 우리가 함께 정의한 객체를 항상 참조할 것이다.
+      // 즉 모두 정확히 동일한 객체를 참조할 수 있다.
+      // console.log("this", this);
+      // Printer {message: 'This works!'}
+      // message: "This works!"
+      // showMessage: ƒ ()
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  // 새로운 descriptor 객체를 반환하여 이전의 descriptor를 덮어쓰게 된다.
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+p.showMessage(); // This works!
+const button = document.querySelector("button");
+
+button?.addEventListener("click", p.showMessage);
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    // 해당 클래스 이름의 기존 key-value 를 가져온다
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    // 해당 클래스 이름의 기존 key-value 를 가져온다
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
+  };
+}
+
+// 등록된 모든 validator를 조사한 후 찾아낸 validator로 다양한 로직을 실행한다.
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    console.log(prop);
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseform = document.querySelector("form")!;
+courseform.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert("Invalid input, please try again!");
+    return;
+  }
+  console.log(createdCourse);
+});
